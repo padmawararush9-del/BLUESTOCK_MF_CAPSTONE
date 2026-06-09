cb6a4eb576f698280b84e24ae167863ec5b552e1
Edu9ights <padmawararush9@gmail.com>
2026-06-09 13:17:27 +0530

    Day 6: Added advanced analytics modules

---
+++ scripts/recommender.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+funds = pd.read_csv("../data/processed/clean_performance.csv")
+
+risk_appetite = input(
+    "Enter Risk Appetite (Low/Moderate/High): "
+)
+
+recommendation_table = (
+    funds[
+        funds["risk_grade"].str.lower()
+        == risk_appetite.lower()
+    ]
+    .sort_values(
+        "sharpe_ratio",
+        ascending=False
+    )
+    .head(3)
+    [
+        [
+            "amfi_code",
+            "scheme_name",
+            "risk_grade",
+            "sharpe_ratio",
+            "return_3yr_pct"
+        ]
+    ]
+)
+
+print("\nTop 3 Recommended Funds:\n")
+print(recommendation_table.to_string(index=False))
